72059e685c1bacae21f4f4b041a8bb52cc065121
chrisxjacobi <chrisxjacobi@gmail.com>
2017-11-30 10:30:57 -0600

    added js to try hover text displaying, having issues

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+//var e = document.querySelector('#picture');
+//
+//e.onmouseover = function() {
+//    document.getElementById('picture').style.display = 'none';
+//}
+//
+//e.onmouseout = function() {
+//    document.getElementById('picture').style.display = 'none';
+//}
